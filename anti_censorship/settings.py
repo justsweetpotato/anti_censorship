5556492a0550d00cb6f81a0fbe5c1cf7d5e2991d
sweetpotato <justsweetpotato@gmail.com>
2019-08-25 17:16:44 +0800

    fixed bug

--- anti_censorship/settings.py
+++ anti_censorship/settings.py
@@ -113,11 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
 
 # if os.getcwd() == '/app':
 #     import dj_database_url
