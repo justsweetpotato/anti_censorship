16df97a735631d9a0f785cdc88de9eb29e741529
sweetpotato <justsweetpotato@gmail.com>
2019-08-26 02:08:23 +0800

    fixed bug

--- text/data.py
+++ text/data.py
@@ -77,6 +77,8 @@ def sub(msg):
     msg = re.sub('[\)）]', '︶', msg)
     msg = re.sub('[\[「]', '﹁', msg)
     msg = re.sub('[\]」]', '﹂', msg)
+    msg = re.sub('{', '︷', msg)
+    msg = re.sub('}', '︸', msg)
     msg = re.sub('<', '︿', msg)
     msg = re.sub('>', '﹀', msg)
     msg = re.sub('a', 'ａ', msg)
