3fc1ace0fddc50c50fcd7c9f021a7f93eef56de9
sweetpotato <justsweetpotato@gmail.com>
2019-08-26 16:08:35 +0800

    fixed bug

--- anti_censorship/settings.py
+++ anti_censorship/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -114,9 +114,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 
 
