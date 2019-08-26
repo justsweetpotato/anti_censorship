d27bc2e3722dcc6e3994d27fd82fa16b20001344
sweetpotato <justsweetpotato@gmail.com>
2019-08-26 15:03:22 +0800

    fixed bug

--- anti_censorship/settings.py
+++ anti_censorship/settings.py
@@ -117,7 +117,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
 
 # if os.getcwd() == '/app':
 #     import dj_database_url
