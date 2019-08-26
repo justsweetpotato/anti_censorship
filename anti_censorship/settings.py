c6e5440df34dfe38699881e135d3d41d8c7aa53b
sweetpotato <justsweetpotato@gmail.com>
2019-08-26 14:59:34 +0800

    review style

--- anti_censorship/settings.py
+++ anti_censorship/settings.py
@@ -113,9 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'anti_censorship/static')
 
